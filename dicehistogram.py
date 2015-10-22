9b3c741d211789acbfe69593b5d1dd6aa3cea601
Mark Fickett <mark.fickett@gmail.com>
2015-10-22 18:09:17 -0400

    Use a mask area for ROI and compute by diff from a reference instead of by greenscrren.

--- dicehistogram.py
+++ dicehistogram.py
@@ -10,14 +10,24 @@ import PIL.ImageDraw
 import collections
 import os
 
-RAW_DIR = 'capture'
+RAW_DIR = 'capture/151021autoroll'
 CROPPED_DIR = 'crop'
 # All cropped images must have uniform size, for machine learning input.
-EDGE_CROPPED = 310
+EDGE_CROPPED = 620
+
+# Photo where the area the die might be in is pure red.
+MASK_IMAGE_FILENAME = 'DSC_6667_redmask.JPG'
+# Photo taken of the area without a die at all.
+REFERENCE_IMAGE_FILENAME = 'DSC_6669.JPG'
 
 COMPARISON_SIZE = EDGE_CROPPED / 6
 OFFSET_SEARCH = 6
+DIFF_THRESHOLD = 150
 DISTANCE_THRESHOLD = 90000
+
+
+def _Summarize(name, image):
+  print name, image.mode, image.size, image.format
 
 
 def TrimOutliersGetExtrema(coordinates, upper_bound_inclusive):
@@ -47,37 +57,80 @@ def TrimOutliersGetExtrema(coordinates, upper_bound_inclusive):
 
   return low, high
 
+global mask_image
+mask_image = None
+def PrepareMask(mask_filename):
+  global mask_image
+  if not mask_image:
+    mask_source_image = PIL.Image.open(mask_filename)
+    _Summarize('mask', mask_source_image)
+    w, h = mask_source_image.size
+    mask_image = PIL.Image.new('1', mask_source_image.size, None)
+    alpha = []
+    tenths = w * h / 10
+    for n, (r, g, b) in enumerate(mask_source_image.getdata()):
+      y = n / w
+      x = n % w
+      if n % tenths == 0:
+        print (x, y)
+      if (r, g, b) == (254, 0, 0):
+        alpha.append(1)
+      else:
+        alpha.append(0)
+    mask_image.putdata(alpha)
+  return mask_image
 
-def ExtractSubject(in_filename, out_filename):
+
+global reference_image
+reference_image = None
+def GetReference(reference_filename, mask):
+  global reference_image
+  if not reference_image:
+    reference_image = PIL.Image.open(reference_filename)
+    _Summarize('ref', reference_image)
+    white = PIL.Image.new('RGB', reference_image.size, 'white')
+    reference_image = PIL.Image.composite(reference_image, white, mask)
+    _Summarize('ref masked', reference_image)
+  return reference_image
+
+
+def ExtractSubject(
+    in_filename,
+    out_filename,
+    reference_filename,
+    mask_filename):
   print in_filename, out_filename
   image = PIL.Image.open(in_filename)
-  print image.mode, image.size, image.format
+  _Summarize('input', image)
   w, h = image.size
 
-  image_data = image.getdata()
-  out_image = PIL.Image.new(image.mode, image.size)
+  white = PIL.Image.new('RGB', image.size, 'white')
+  mask = PrepareMask(mask_filename)
+  reference = GetReference(reference_filename, mask)
+  image = PIL.Image.composite(image, white, mask)
+  diff = PIL.ImageChops.difference(reference, image)
 
   tenths = (h * w) / 10
   matched_x = []
   matched_y = []
-  for n, (r, g, b) in enumerate(image_data):
+  for n, (dr, dg, db) in enumerate(diff.getdata()):
     y = n / w
     x = n % w
     if n % tenths == 0:
       print (x, y)
-    if max(r, b) >= g:
-      out_image.putpixel((x, y), (r, g, b))
+    if sum((dr, dg, db)) >= DIFF_THRESHOLD:
+      diff.putpixel((x, y), (254, 0, 0))
       matched_x.append(x)
       matched_y.append(y)
-    #else:
-    #  out_image.putpixel((x, y), (r, g, b))
+  diff.show()
 
+  # NEXT: Rework bound computation.
   min_x, max_x = TrimOutliersGetExtrema(matched_x, w - 1)
   min_y, max_y = TrimOutliersGetExtrema(matched_y, h - 1)
   bound = (min_x, min_y, max_x, max_y)
   print bound
-  out_image = out_image.crop(bound)
-  print out_image.size
+  out_image = image.crop(bound)
+  _Summarize('output', out_image)
   out_image.save(out_filename)
 
 
@@ -154,14 +207,18 @@ def BuildClusterSummaryImage(clusters):
 if __name__ == '__main__':
   EXTRACT = 0
   CLUSTER = 1
-  run_stages = (CLUSTER,)
+  #run_stages = (EXTRACT, CLUSTER,)
+  run_stages = (EXTRACT,)
   if EXTRACT in run_stages:
-    for raw_image_filename in os.listdir(RAW_DIR):
-      if not raw_image_filename.endswith('jpg'):
+    #for raw_image_filename in os.listdir(RAW_DIR):
+    for raw_image_filename in ('DSC_6769.JPG', 'DSC_6882.JPG', 'DSC_6995.JPG', 'DSC_7108.JPG', 'DSC_7221.JPG'):
+      if not raw_image_filename.lower().endswith('jpg'):
         continue
       ExtractSubject(
           os.path.join(RAW_DIR, raw_image_filename),
-          os.path.join(CROPPED_DIR, raw_image_filename))
+          os.path.join(CROPPED_DIR, raw_image_filename),
+          os.path.join(RAW_DIR, REFERENCE_IMAGE_FILENAME),
+          os.path.join(RAW_DIR, MASK_IMAGE_FILENAME))
   if CLUSTER in run_stages:
     clusters = []
     for cropped_image_filename in os.listdir(CROPPED_DIR):
