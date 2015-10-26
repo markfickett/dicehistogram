2de7f4cb2a5c94daa3422ee883445468aa3c9631
Mark Fickett <mark.fickett@gmail.com>
2015-10-25 23:18:36 -0400

    switch grouping to use OpenCV

--- group.py
+++ group.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy
+
 import PIL
 import PIL.Image
 import PIL.ImageChops
@@ -9,142 +12,69 @@ import os
 
 import common
 
-# Categorization parameters.
-# Before comparison, the cropped image is sized down by this factor (and
-# converted to grayscale).
-COMPARISON_RESIZE_FACTOR = 10
-# Before comparison, the resized image is center-cropped with a square.
-COMPARISON_CENTER_CROP_SIZE = 550 / COMPARISON_RESIZE_FACTOR
-# Before comparison, the resized/cropped image is thresholded at this value to
-# convert it to a bitmap, fully black or white image.
-COMPARISON_THRESHOLD = 180
-# During comparison, search +/- this many pixels translation for a match.
-OFFSET_SEARCH = 20 / COMPARISON_RESIZE_FACTOR
-OFFSET_SEARCH_INCREMENT = 1
-# During comparison the image is rotated 360d, this many degrees at a time.
-ROTATION_SEARCH_INCREMENT = 6
-# Erosion (removing pixels from a diff where those pixels have matches nearby)
-# removes thin outlines. Such outlines are common when a match is offset just
-# slightly. But eroding leaves blobs (indicative of larger mismatches)  intact.
-# Try erosion when at most this many pixels differ.
-DO_EROSION_THRESHOLD = 210
-# Pixels with this many exposed sides (including diagonals) get eroded.
-EROSION_THRESHOLD = 4
-# Absolute difference (number of differing pixels) at/below which eroded
-# comparisons are considered a match.
-DISTANCE_THRESHOLD = 10
+MATCH_COUNT_THRESHOLD = 40
 
 # Edge size for the otherwise unaltered image in the summary image.
 SUMMARY_MEMBER_IMAGE_SIZE = 150
-SUMMARY_MAX_MEMBERS = 8
+SUMMARY_MAX_MEMBERS = 50
 
 
 class ImageComparison(object):
-  _resized_circle_mask = None
+  detector = cv2.AKAZE_create()
+  matcher = cv2.BFMatcher(cv2.NORM_HAMMING)
 
-  def __init__(self, image, filename):
-    self.image = image.resize(
+  def __init__(self, in_filename):
+    self.filename = in_filename
+    self.image = PIL.Image.open(in_filename).resize(
         (SUMMARY_MEMBER_IMAGE_SIZE, SUMMARY_MEMBER_IMAGE_SIZE))
-    self.filename = filename
-    self.distance = None
-    w = image.size[0] / COMPARISON_RESIZE_FACTOR
-    center = w / 2
-    r = COMPARISON_CENTER_CROP_SIZE / 2
-    self.resized = image.resize((w, w), resample=PIL.Image.BILINEAR)
-    self.resized = (self.resized
-      .crop((center - r, center - r, center + r, center + r))
-      .convert(mode='L')
-      .point(lambda x: 254 if x > COMPARISON_THRESHOLD else 0)
-      .convert(mode='1'))
-    self.diff = None
+    cv_image = cv2.imread(in_filename, 0)
+    if cv_image is None:
+      raise RuntimeError('OpenCV could not open %s' % in_filename)
+    self.features, self.descriptors = self.detector.detectAndCompute(
+        cv_image, None)
+    self.match_count = 0
 
-  @classmethod
-  def GetCenterCropCircleMask(cls):
-    """Returns a circle mask for the resized comparison/diff image."""
-    if cls._resized_circle_mask is None:
-      w = COMPARISON_CENTER_CROP_SIZE
-      dx = OFFSET_SEARCH
-      cls._resized_circle_mask = PIL.Image.new('1', (w, w), 0)
-      draw = PIL.ImageDraw.Draw(cls._resized_circle_mask)
-      draw.ellipse((dx, dx, w - dx, w - dx), fill=1)
-    return cls._resized_circle_mask
+
+def FilterMatches(features_a, features_b, raw_matches, ratio=0.75):
+  matching_features_a, matching_features_b = [], []
+  for m in raw_matches:
+    if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+      matching_features_a.append(features_a[m[0].queryIdx])
+      matching_features_b.append(features_b[m[0].trainIdx])
+  p1 = numpy.float32([kp.pt for kp in matching_features_a])
+  p2 = numpy.float32([kp.pt for kp in matching_features_b])
+  return p1, p2, zip(matching_features_a, matching_features_b)
 
 
 def AssignToCluster(in_filename, clusters):
-  image = ImageComparison(PIL.Image.open(in_filename), in_filename)
-  best_distance = float('Inf')
+  image = ImageComparison(in_filename)
+  best_match_count = 0
   best_members = None
-  best_diff = None
   for representative, members in clusters:
-    distance, diff = FindErodedDistance(image, representative)
-    print '%s - %s = %d' % (
-        image.filename, representative.filename, distance)
-    if distance < best_distance:
-      best_distance = distance
-      best_diff = diff
+    raw_matches = ImageComparison.matcher.knnMatch(
+        image.descriptors,
+        trainDescriptors=representative.descriptors,
+        k=2)
+    p1, p2, matching_feature_pairs = FilterMatches(
+        image.features, representative.features, raw_matches)
+    match_count = min(len(p1), len(p2))
+    """
+    if len(p1) >= 4:
+      unused_homography, matched_points = cv2.findHomography(
+          p1, p2, cv2.RANSAC, 5.0)
+      match_count = numpy.sum(matched_points)  # inliers
+    print '%s match %s = %d' % (
+         image.filename, representative.filename, match_count)
+    """
+    if match_count > best_match_count:
+      best_match_count = match_count
       best_members = members
-      if distance <= DISTANCE_THRESHOLD:
-        break
-  image.distance = best_distance
-  image.diff = best_diff
-  if best_members is None or best_distance > DISTANCE_THRESHOLD:
+  image.match_count = best_match_count
+  if best_members is None or best_match_count < MATCH_COUNT_THRESHOLD:
     print '%s starts new cluster' % image.filename
     clusters.append((image, []))
   else:
     best_members.append(image)
-
-
-def FindErodedDistance(image, representative):
-  best_distance = float('Inf')
-  best_diff = None
-  for r in xrange(0, 360, ROTATION_SEARCH_INCREMENT):
-    rotated = representative.resized.rotate(r)
-    for dx in xrange(-OFFSET_SEARCH, OFFSET_SEARCH, OFFSET_SEARCH_INCREMENT):
-      for dy in xrange(-OFFSET_SEARCH, OFFSET_SEARCH, OFFSET_SEARCH_INCREMENT):
-        abs_diff = PIL.ImageChops.difference(
-            PIL.ImageChops.offset(rotated, dx, dy),
-            image.resized)
-        abs_diff = PIL.ImageChops.logical_and(
-            abs_diff, ImageComparison.GetCenterCropCircleMask())
-        diff_sum = GetErodedSum(abs_diff)
-        if diff_sum < best_distance:
-          best_distance = diff_sum
-          best_diff = abs_diff
-          if best_distance <= DISTANCE_THRESHOLD:
-            return best_distance, best_diff
-  return best_distance, best_diff
-
-
-def GetErodedSum(diff):
-  diff_data = list(diff.getdata())
-  basic_sum = sum(diff_data) / 255
-  if basic_sum > DO_EROSION_THRESHOLD:
-    return basic_sum
-
-  data_iter = iter(diff_data)
-  data_matrix = []
-  w, h = diff.size
-  for x in xrange(w):
-    row = []
-    for y in xrange(h):
-      row.append(data_iter.next())
-    data_matrix.append(row)
-
-  interior_count = 0
-  for x in xrange(w):
-    for y in xrange(h):
-      if not data_matrix[x][y]:
-        continue
-      num_missing = 0
-      for dx in xrange(-1, 2):
-        for dy in xrange(-1, 2):
-          if not data_matrix[(x + dx) % w][(y + dy) % h]:
-            num_missing += 1
-        if num_missing >= EROSION_THRESHOLD:
-          break
-      if num_missing < EROSION_THRESHOLD:
-        interior_count += 1
-  return interior_count
 
 
 def BuildClusterSummaryImage(clusters, skip_len):
@@ -166,14 +96,9 @@ def BuildClusterSummaryImage(clusters, skip_len):
       x = j * large_edge
       summary_image.paste(member.image, (x, y))
       draw.text((x, y), member.filename[skip_len:])
-      top_for_resized = y + (large_edge - member.resized.size[0])
-      summary_image.paste(member.resized, (x, top_for_resized))
-      if member.diff is not None:
-        summary_image.paste(
-            member.diff, (x + member.resized.size[0], top_for_resized))
-      if member.distance is not None:
-        draw.text((x, y + 20), str(member.distance))
-    draw.text((0, y + 40), '%d members' % (len(members) + 1))
+      draw.text((x, y + 20), 'features: %d' % len(member.features))
+      draw.text((x, y + 40), 'matches: %d' % member.match_count)
+    draw.text((0, y + 60), 'members: %d' % (len(members) + 1))
   return summary_image
 
 
