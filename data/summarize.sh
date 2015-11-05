164f2426b7a59e755f94d7b0cad684b1d09bd374
Mark Fickett <mark.fickett@gmail.com>
2015-11-04 23:04:54 -0500

    Add summary data and overall summary script to repo, update gitignore.

---
+++ data/summarize.sh
@@ -0,0 +1,17 @@
+# from Chessex small-box set purchased 2015
+../summarize.py 151026d4chessexgeminicoppersteel 4 1 2 3 2 2 3 2 2 3 2 2 3 2 2 3 2 2 2 2 # TODO new/more data
+../summarize.py 151026d6chessexgeminicoppersteel 5 6 4 1 2 1 1 3 1 2 1 6 1 1 5 1 1 # TODO new/more data
+../summarize.py 151026d8chessexgeminicoppersteel 3 8 1 6 5 7 4 2 7 1 4 7 2 3 1 4 3 4 1 1 4 1 1
+# TODO d10 / d100 (first imagery)
+../summarize.py 151027d12chessexgeminicoppersteel 9 7 12 1 4 10 8 5 11 3 6 2 4 4 7 6 8 4 # TODO new/more data
+../summarize.py 151023d20chessexgeminicoppersteel 2 1 9 18 20 13 11 14 3 10 15 7 6 5 19 17 8 16 12 4 7 1 4 1 7 8 7 11 4 3
+
+# from highcitybooks 35-pack of Wiz Dice purchased 2015
+#../summarize.py 151028d20chessexopaquepurple 6 9 4 17 3 12 18 5 20 10 19 8 2 13 15 11 14 1 7 16 13  # N = 2606
+../summarize.py 151031d20chessexopaquepurple/ 3 17 12 18 6 4 19 9 2 14 11 8 15 7 10 5 1 16 20 13 17 4  # N = 8300
+../summarize.py 151029d20chessextranslucentblue/ 8 11 2 13 5 6 14 12 19 9 16 15 4 18 1 7 20 10 20 3 17 17 17 19 3 13 7 3 17 16 3 17
+
+# blue-green: purchased 201x probably Chessex
+../summarize.py 151103d20greenmarbled 5 4 11 14 17 15 6 16 20 9 19 8 18 12 10 3 2 13 7 1 2 7 7 7 1 7 1 7 1 9 7 7 9 1 16 7 20 14 1
+# red: purchased 199x, manufacturer unknown
+../summarize.py 151104d20red199x/ 11 6 12 9 16 4 5 18 10 15 17 13 8 2 1 19 14 7 20 3 3 10 16
