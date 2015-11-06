bd09466fc0688e7b760365695740ead731179e83
Mark Fickett <mark.fickett@gmail.com>
2015-11-05 21:04:37 -0500

    Add new Wiz Dice data.

--- data/summarize.sh
+++ data/summarize.sh
@@ -7,11 +7,15 @@
 ../summarize.py 151023d20chessexgeminicoppersteel 2 1 9 18 20 13 11 14 3 10 15 7 6 5 19 17 8 16 12 4 7 1 4 1 7 8 7 11 4 3
 
 # from highcitybooks 35-pack of Wiz Dice purchased 2015
-#../summarize.py 151028d20chessexopaquepurple 6 9 4 17 3 12 18 5 20 10 19 8 2 13 15 11 14 1 7 16 13  # N = 2606
-../summarize.py 151031d20chessexopaquepurple/ 3 17 12 18 6 4 19 9 2 14 11 8 15 7 10 5 1 16 20 13 17 4  # N = 8300
-../summarize.py 151029d20chessextranslucentblue/ 8 11 2 13 5 6 14 12 19 9 16 15 4 18 1 7 20 10 20 3 17 17 17 19 3 13 7 3 17 16 3 17
+#../summarize.py 151028d20wizdiceopaquepurple 6 9 4 17 3 12 18 5 20 10 19 8 2 13 15 11 14 1 7 16 13  # N = 2606
+../summarize.py 151031d20wizdiceopaquepurple/ 3 17 12 18 6 4 19 9 2 14 11 8 15 7 10 5 1 16 20 13 17 4  # N = 8300
+../summarize.py 151029d20wizdicetranslucentblue/ 8 11 2 13 5 6 14 12 19 9 16 15 4 18 1 7 20 10 20 3 17 17 17 19 3 13 7 3 17 16 3 17
+# borrowed wiz dice
+../summarize.py 151105d20wizdiceyellow/ 18 2 8 3 14 15 6 1 12 19 7 9 16 10 11 20 5 4 13 17 1 11 20 9 8 12 12 1 10
+../summarize.py 151105d20wizdiceblue 18 17 4 6 3 15 19 7 9 8 14 11 10 12 1 5 2 16 20 13 13 1 1 16 7 19 1 4 9 17 3 1
+
 
 # blue-green: purchased 201x probably Chessex
-../summarize.py 151103d20greenmarbled 5 4 11 14 17 15 6 16 20 9 19 8 18 12 10 3 2 13 7 1 2 7 7 7 1 7 1 7 1 9 7 7 9 1 16 7 20 14 1
+../summarize.py 151103d20chessexgreenmarbled 5 4 11 14 17 15 6 16 20 9 19 8 18 12 10 3 2 13 7 1 2 7 7 7 1 7 1 7 1 9 7 7 9 1 16 7 20 14 1
 # red: purchased 199x, manufacturer unknown
 ../summarize.py 151104d20red199x/ 11 6 12 9 16 4 5 18 10 15 17 13 8 2 1 19 14 7 20 3 3 10 16
