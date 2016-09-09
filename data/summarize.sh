ee0766f31aa97533492cc8e40691ae85435d0b0e
Mark Fickett <mark.fickett@gmail.com>
2016-09-08 21:54:22 -0400

    Add labels.csv for all existing labeled datasets, replace summarize.sh with a loop over those CSVs.

--- data/summarize.sh
+++ data/summarize.sh
@@ -1,65 +1,15 @@
-# from Chessex small-box set purchased 2015
-../summarize.py 151026d4chessexgeminicoppersteel 4 1 2 3 2 2 3 2 2 3 2 2 3 2 2 3 2 2 2 2
-../summarize.py 151114d6chessexgeminicoppersteel 3 4 5 2 6 1 5 6 1 1 1
-../summarize.py 151026d8chessexgeminicoppersteel 3 8 1 6 5 7 4 2 7 1 4 7 2 3 1 4 3 4 1 1 4 1 1
-# TODO d10 / d100 (first imagery)
-../summarize.py 151027d12chessexgeminicoppersteel 9 7 12 1 4 10 8 5 11 3 6 2 4 4 7 6 8 4 # TODO new/more data
-../summarize.py 151023d20chessexgeminicoppersteel 2 1 9 18 20 13 11 14 3 10 15 7 6 5 19 17 8 16 12 4 7 1 4 1 7 8 7 11 4 3
-# from Chessex bag of 6 assorted d20s purchased 2015 Nov
-../summarize.py 151109d20chessexpurplegray 17 3 10 4 12 11 16 5 9 18 6 15 14 2 1 13 7 20 8 19 4 1 20 4 6 20
-../summarize.py 151111d20chessexredorange 18 4 6 17 10 5 3 15 11 16 12 20 14 9 2 19 8 1 13 7
-../summarize.py 151111d20chessexyellow 18 12 11 6 15 3 10 8 9 14 2 16 5 20 4 19 13 7 17 1 2 18 1 20 7
-# purchased 201x probably Chessex
-../summarize.py 151103d20chessexgreenmarbled 5 4 11 14 17 15 6 16 20 9 19 8 18 12 10 3 2 13 7 1 2 7 7 7 1 7 1 7 1 9 7 7 9 1 16 7 20 14 1
-
-# purchased 199x, manufacturer unknown
-../summarize.py 151104d20red199x/ 11 6 12 9 16 4 5 18 10 15 17 13 8 2 1 19 14 7 20 3 3 10 16
-
-# from highcitybooks 35-pack of Wiz Dice purchased 2015
-#../summarize.py 151028d20wizdiceopaquepurple 6 9 4 17 3 12 18 5 20 10 19 8 2 13 15 11 14 1 7 16 13  # N = 2606
-../summarize.py 151031d20wizdiceopaquepurple/ 3 17 12 18 6 4 19 9 2 14 11 8 15 7 10 5 1 16 20 13 17 4  # N = 8300
-../summarize.py 151113d6wizdiceopaquepurple 3 6 2 5 4 4 5 6 1 1 1 1 1 1 3 1 2 2 --repeat 1
-../summarize.py 151029d20wizdicetranslucentblue/ 8 11 2 13 5 6 14 12 19 9 16 15 4 18 1 7 20 10 20 3 17 17 17 19 3 13 7 3 17 16 3 17
-../summarize.py 151114d6wizdicetranslucentblue 5 6 3 4 2 6 --repeat 1
-# borrowed wiz dice
-../summarize.py 151105d20wizdiceyellow/ 18 2 8 3 14 15 6 1 12 19 7 9 16 10 11 20 5 4 13 17 1 11 20 9 8 12 12 1 10
-../summarize.py 151105d20wizdiceblue 18 17 4 6 3 15 19 7 9 8 14 11 10 12 1 5 2 16 20 13 13 1 1 16 7 19 1 4 9 17 3 1
-../summarize.py 151106d20wizdicetranslucentblue 20 14 18 2 7 4 19 8 13 15 6 9 12 5 16 10 11 3 17 1 1 7 7 4 19
-
-# GameScience d20 pair purchased 2015 Nov; black die before and after trimming
-../summarize.py 151109d20gamescienceblackgold 3 11 19 16 2 5 7 4 18 20 10 15 12 6 8 1 13 9 17 14 1 17 17 18 14 19 18 5 13 8 3 10 7 16
-../summarize.py 151109d20gamescienceblackgoldtrimmed 13 18 2 1 17 16 3 5 6 10 20 7 14 19 12 11 15 9 4 8 4 8 4 4 11 17 8 15
-../summarize.py 151119d20gamesciencewhite 16 1 15 11 20 17 18 5 9 12 2 14 7 6 4 13 10 19 8 3 10 7
-
-# Crystal Caste purchased 2015 Nov
-../summarize.py 151111d20crystalcasteclearblack 8 1 20 19 7 2 14 12 3 13 18 5 6 4 9 11 15 10 16 17 17 17 17 17 7 17 17 17 11 19 17 17 6 17 17 15 17
-../summarize.py 151112d20crystalcastetranslucentorange 2 20 8 19 13 12 15 16 6 1 4 11 18 9 14 7 3 5 1 7 14 3 8 19 17 10 10 17 17 17 10 10 10 10 17 10 17 17 17 7 17 17 15 17 7 17 17 17 10 17 17 10 4 17 20 2
-../summarize.py 151111d8crystalcasteclearblack 5 4 2 6 8 7 3 --repeat 1
-../summarize.py 151114d6crystalcastetranslucentorange 5 2 3 6 4 4 1 2 1 2 1 1 3 5 1 1 1 6 2 2 2 2 2 5 2 6  --repeat 1
-../summarize.py 151114d6crystalcasteclearblack 3 5 6 4 2 --repeat 1
-../summarize.py 151126d4crystalcasteblack 2 4 1 3 1 3 4 1 3 2 2 3 3 2 4 2 2 3 3 4 4 3 3 4 4 1 4 4 1 2 2 1 4 3 1 2 2 3 3 4 4 2 3 3 2 2 3 4 4 2
-
-# Koplow, from set of 10 d20s purchased 2015 Nov
-../summarize.py 151113d20koplowblue 11 20 18 9 14 5 15 4 17 10 1 16 8 13 12 19 3 2 7 6 6 9 1 11
-../summarize.py 151113d20koplowgreen 16 18 14 4 10 17 11 5 20 2 12 13 6 1 3 19 15 8 7 9 9 7 4 10 4 2 2 7 3 16 6 10 3
-../summarize.py 151115d20koplowgreenother 14 3 20 8 11 12 19 1 15 18 5 4 9 16 7 2 13 17 10 6 10 6 2
-
-# Koplow d6s, donated via Dice Lab 2015 Dec
-../summarize.py 160104koplowd6/ 6 2 3 4 1 5 3 6
-../summarize.py 160110koplowd6/ 6 1 4 3 5 2
-../summarize.py 160110koplowd6b/ 1 3 4 5 6 2
-
-# Dice Lab Skew Dice, donated 2015 Dec
-../summarize.py 160101skewd12reflected/ 9 3 8 11 5 10 4 6 2 12 1 7 7 7 7 7 7 7 1 7 7 7 7 7 7 1 7 7 7 7 8 11 7 7 1 7 1 10 7 2 7 2 10 1 8 7 2 1 7 7 7 7 8 9 7 1 7 8 11 4 1 7 11 1 2 7 1 4 8 1 7 7 4 7 2 10
-../summarize.py 160102skewd12reflected/ 3 9 11 8 10 5 4 6 2 1 7 12 7
-../summarize.py 160103skewd12/ 3 11 9 8 10 6 7 5 2 4 12 1 7 7 1 7
-../summarize.py 160104skewd12/ 9 3 8 11 5 10 6 4 2 7 12 1 2
-# The below Skew D6 categorizations have a handful of known errors.
-../summarize.py 160110skewd6cw 4 2 5 3 6 1 1
-../summarize.py 160111skewd6ccw 4 3 2 5 6 1 1
-../summarize.py 160111skewd6cw 4 5 3 2 1 6 5
-../summarize.py 160112skewd6ccw 4 2 3 5 6 1 1 6
-
-# Settlers of Catan's included dice
-../summarize.py 160117catanyellowd6 4 2 6 3 1 5 2 2 4
-../summarize.py 160117catanredd6 6 4 2 1 5 3 6 1
+#!/bin/bash
+for data_dir in *
+do
+  if [ ! -d "${data_dir}" ]
+  then
+    continue
+  fi
+  if [ -f "${data_dir}/labels.csv" ]
+  then
+    ../summarize.py "$data_dir"
+  else
+    echo No labels file in ${data_dir}.
+  fi
+  echo
+done
